a2e78da751f7166ac23f0d2b92912997453bdcd1
Geraldo Neto <geraldo.linkedit@gmail.com>
2025-09-23 14:45:56 -0300

    projeto-v01

---
+++ assets/js/configs.js
@@ -0,0 +1,11 @@
+// assets/js/configs.js
+export const msoConfigs = {
+  // Animação isolada (não-MSO): #item51170
+  item51170: {
+    default: {
+      containerId: 'item51170',
+      path: './assets/json/anima-capa.json',
+      loop: true // SOMENTE esta em loop
+    }
+  }
+};
