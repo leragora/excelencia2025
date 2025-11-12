8dba557fd35adfc78dda6d8218f34254e9582561
Geraldo Neto <geraldo.linkedit@gmail.com>
2025-11-12 13:09:17 -0300

    revisão-v01

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -7,5 +7,37 @@ export const msoConfigs = {
       path: './assets/json/anima-capa.json',
       loop: true // SOMENTE esta em loop
     }
-  }
+  },
+
+  item60168: {
+    'sexo': {
+      containerId: 'item60146',
+      path: './assets/json/sexo.json'
+    },
+    'idade': {
+      containerId: 'item60134',
+      path: './assets/json/idade.json'
+    },
+    'religiao': {
+      containerId: 'item59655',
+      path: './assets/json/religiao.json'
+    },
+    'escolaridade': {
+      containerId: 'item60094',
+      path: './assets/json/escolaridade.json'
+    },
+    'classe': {
+      containerId: 'item60104',
+      path: './assets/json/classe.json'
+    },
+    'moradia': {
+      containerId: 'item60115',
+      path: './assets/json/moradia.json'
+    },
+    'mercado': {
+      containerId: 'item60125',
+      path: './assets/json/mercado.json'
+    }
+  },
+
 };
